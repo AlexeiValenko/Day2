2215840bf39608e07b9448b6789582a4e4f43d8a
AlexeiValenko <thrower@gmail.com>
2017-01-16 14:50:09 +0200

    day2

---
+++ switch check.js
@@ -0,0 +1,3 @@
+/**
+ * Created by py on 1/16/17.
+ */
---
+++ switchCheck.js
@@ -0,0 +1,17 @@
+/**
+ * Created by py on 1/16/17.
+ */
+var check = 3;
+switch (check){
+    case 1: console.log('what????');
+        break;
+    case 2: console.log('what????');
+        break;
+    case 3: console.log('realy 3');
+        break;
+    case "3": console.log('interesting');
+        break;
+    default: console.log('what????');
+        break;
+
+}
